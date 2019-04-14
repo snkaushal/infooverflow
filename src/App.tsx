dc7a10413461ec7b771a344557b5745d791fb9a3
Sahil <skaushal@breadware.com>
2019-04-14 12:31:57 +0530

    Added Route and Sass packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        Landing
+        <Link to="/notlanding">Non Landing</Link>
+      </div>
+    )
+  }
+}
+
+class NonLanding extends Component {
+  render() {
+    return (
+      <div>
+        non Landing
+        <Link to="/">Landing</Link>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/notlanding" component={NonLanding} />
+        </Router>
       </div>
     );
   }
