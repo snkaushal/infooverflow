92c88cd9f15366f42bdc70312b902ec2be77683b
Sahil <skaushal@breadware.com>
2019-04-18 21:55:27 +0530

    added second and third section of landing page

--- src/app/views/Landing/index.tsx
+++ src/app/views/Landing/index.tsx
@@ -5,25 +5,35 @@ import RotatingCard from '../../../lib/components/RotatingCard';
 
 // Mock Data
 const categories = [
-  {category: 'Design', 
-    icon: 'fa-paint-brush', 
-    description: 'Design is a work process which has a user perspective and '+
-    'drives development based on your specific customers needs'},
-  {category: 'Programming', 
-    icon: 'fa-code', 
-    description: 'Programming is the process of taking an algorithm and encoding it into a notation, '+
-    'a programming language, so that it can be executed by a computer'},
-  {category: 'GK',
-   icon: 'fa-book', 
-   description: 'General Knowledge is knowledge of a broad range of facts about various subjects.'},
-  {category: 'Marketing', 
-  icon: 'fa-pie-chart', 
-  description: 'Marketing the action or business of promoting and '+
-  'selling products or services, including market research and advertising.'},
-  {category: 'Politics', 
-  icon: 'fa-handshake-o', 
-  description: 'Politics is the activities associated with the governance of '+
-  'a country or area, especially the debate between parties having power.'}
+  {
+    category: 'Design',
+    icon: 'fa-paint-brush',
+    description: 'Design is a work process which has a user perspective and ' +
+      'drives development based on your specific customers needs'
+  },
+  {
+    category: 'Programming',
+    icon: 'fa-code',
+    description: 'Programming is the process of taking an algorithm and encoding it into a notation, ' +
+      'a programming language, so that it can be executed by a computer'
+  },
+  {
+    category: 'GK',
+    icon: 'fa-book',
+    description: 'General Knowledge is knowledge of a broad range of facts about various subjects.'
+  },
+  {
+    category: 'Marketing',
+    icon: 'fa-pie-chart',
+    description: 'Marketing the action or business of promoting and ' +
+      'selling products or services, including market research and advertising.'
+  },
+  {
+    category: 'Politics',
+    icon: 'fa-handshake-o',
+    description: 'Politics is the activities associated with the governance of ' +
+      'a country or area, especially the debate between parties having power.'
+  }
 ];
 
 interface LandingProps { }
@@ -56,16 +66,36 @@ export default class Landing extends React.Component<LandingProps, LandingState>
           </div>
         </header>
         <main>
-          <div className={classnames('io-landing__section-one')}>
+          <div className={classnames('io-landing__section-one', 'io-marg--center', 'io-lg-pad--bottom')}>
             <h1 className={classnames('io-heading-primary')}>
               You can ask a question in the following categories
             </h1>
-            <div className={classnames('io-landing__section-one-categories', 'io-row')}>
+            <div className={classnames('io-landing__section-one-categories', 'io-row', 'io-sl-pad--bottom')}>
               {categories.map(({ category, icon, description }) => (
-                <RotatingCard 
-                  frontSide={this.frontSideOfCard(icon, category)} 
+                <RotatingCard
+                  frontSide={this.frontSideOfCard(icon, category)}
                   backSide={this.backSideOfCard(description)} />
               ))}
+            </div>
+          </div>
+          <div className={classnames('io-landing__section-two', 'io-sl-marg--vert')}>
+            <div className={classnames('io-landing__quotation-container', 'io-lg-marg--vert', 'io-md-pad')}>
+              <figure className={classnames('io-landing__quotation-image', 'io-md-marg--right')}>
+
+              </figure>
+              <figcaption>
+                Withholding information is the essence of tyranny.
+                Control of the flow of information is the tool of the dictatorship.
+              </figcaption>
+            </div>
+            <div className={classnames('io-landing__quotation-container', 'io-lg-marg--vert', 'io-md-pad')}>
+              <figure className={classnames('io-landing__quotation-image', 'io-md-marg--right')}>
+
+              </figure>
+              <figcaption>
+                Withholding information is the essence of tyranny.
+                Control of the flow of information is the tool of the dictatorship.
+                </figcaption>
             </div>
           </div>
         </main>
