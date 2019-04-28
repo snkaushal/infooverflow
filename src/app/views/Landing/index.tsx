f80a09cbc21f86a458d5861d927f861d01c366b0
Sahil <skaushal@breadware.com>
2019-04-28 11:57:36 +0530

    testing a revert commit

--- src/app/views/Landing/index.tsx
+++ src/app/views/Landing/index.tsx
@@ -84,6 +84,7 @@ export default class Landing extends React.Component<LandingProps, LandingState>
 
               </figure>
               <figcaption>
+                This has to be reverted.
                 Withholding information is the essence of tyranny.
                 Control of the flow of information is the tool of the dictatorship.
               </figcaption>
